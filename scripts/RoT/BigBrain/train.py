f50c230d85b6c03603914c45439e8827012d1cca
Adria <a.casamitjana@ucl.ac.uk>
2021-08-02 09:57:30 +0100

    rename RoT methods

--- scripts/RoT/BigBrain/train.py
+++ scripts/RoT/BigBrain/train.py
@@ -12,7 +12,7 @@ from src.callbacks import LRDecay
 from src.utils.tensor_utils import TensorDeformation
 from src.utils.io import DebugWriter, ResultsWriter, create_results_dir, ExperimentWriter, worker_init_fn
 from src.training import RoT
-from scripts.RoT.Allen_labels import configFileAllen
+from scripts.RoT.BigBrain import configFileBigBrain
 
 ####################################
 ######## GLOBAL  PARAMETERS ########
@@ -40,7 +40,7 @@ l_regsmooth = arguments.l_regsmooth
 l_GAN = arguments.l_GAN
 
 rid_list = None
-configFile = configFileAllen
+configFile = configFileBigBrain
 parameter_dict = configFile.CONFIG
 parameter_dict['mask_flag'] = mask_flag
 parameter_dict['clip_grad'] = clip_grad
@@ -130,10 +130,6 @@ G_M = models.ResnetGenerator(
     tanh=True if 'TANH' in parameter_dict['PARENT_DIRECTORY'] else False
 )
 
-# Feature extractor Network
-F_M = models.PatchSampleF(
-    use_mlp=True, device=device, nc=num_nc
-)
 
 registration_M = models.VxmDense(
     nb_unet_features=[parameter_dict['ENC_NF_REG'], parameter_dict['DEC_NF_REG']],
@@ -150,7 +146,7 @@ discriminator_M = models.NLayerDiscriminator(
     device=device, init_type='xavier'
 )
 
-model_dict = {'G_M': G_M, 'D_M': discriminator_M, 'F_M': F_M, 'R_M': registration_M}
+model_dict = {'G_M': G_M, 'D_M': discriminator_M, 'R_M': registration_M}
 
 da_model = TensorDeformation(image_shape, parameter_dict['NONLINEAR'].lowres_size, device)
 
